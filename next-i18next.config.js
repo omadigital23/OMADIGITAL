94149dc4465adbffb78cf91ba44fe2dba40a2541
omadigital23 <omadigital23@gmail.com>
2025-10-13 08:02:20 +0100

    fix: create dedicated about.json namespace for about page translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     default: ['fr'],
   },
   localePath: path.resolve('./public/locales'),
-  ns: ['common', 'legal'],
+  ns: ['common', 'legal', 'about'],
   defaultNS: 'common',
   debug: process.env.NODE_ENV === 'development',
   reloadOnPrerender: process.env.NODE_ENV === 'development',

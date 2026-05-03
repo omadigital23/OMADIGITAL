3a0303d4d66629b018758c7f31cf9de4fc570fc1
SOJIF Development Team <development@sojif-consulting.com>
2026-05-03 06:23:21 +0100

    Use www canonical domain

--- src/lib/site-url.ts
+++ src/lib/site-url.ts
@@ -1,7 +1,5 @@
-export const DEFAULT_SITE_URL = 'https://omadigital.net';
+export const DEFAULT_SITE_URL = 'https://www.omadigital.net';
 
 export function getSiteUrl(): string {
-  const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL || DEFAULT_SITE_URL).replace(/\/+$/, '');
-
-  return siteUrl === 'https://www.omadigital.net' ? DEFAULT_SITE_URL : siteUrl;
+  return (process.env.NEXT_PUBLIC_SITE_URL || DEFAULT_SITE_URL).replace(/\/+$/, '');
 }
